75fcf6d2be2b1107262bf4b6632c2fd7ccb94085
Yuttapichai Paul Linglom <linglomst@gmail.com>
2022-02-18 13:45:01 +0700

    calAge

--- intro-to-js/paul.js
+++ intro-to-js/paul.js
@@ -7,5 +7,20 @@ function askMyname() {
 
 function helloWorld() {
     const h1message = document.getElementById("name-tag").innerHTML;
-    alert(h1message + "ชีวิตยังมีค่าอย่ามาซ่ากับพวกเรา");
+    alert(h1message + " ชีวิตยังมีค่าอย่ามาซ่ากับพวกเรา");
+}
+
+function calAge() {
+    const birthYear = Number(prompt("Please enter your birthyear in AD here"));
+    if (birthYear != null) {
+        const age = (new Date().getFullYear() - birthYear);
+        alert(`สวัสดีปีนี้คุณอายุ ${age} แล้วนะจ๊ะ!`)
+    }
+}
+
+function loadProfile() {
+    const name = "Yuttapichai Paul Linglom";
+    const province = "Suratthani";
+    document.getElementById('my-name').innerHTML = name;
+    document.getElementById('my-province').innerHTML = province;
 }
